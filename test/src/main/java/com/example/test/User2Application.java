84072ad3cf8cdb6abe34a9aa6bb6b68cea00be14
KhanhNhat242 <86726746+KhanhNhat242@users.noreply.github.com>
2023-07-01 16:10:36 +0700

    cau 2 nhung chua xong

---
+++ test/src/main/java/com/example/test/User2Application.java
@@ -0,0 +1,88 @@
+package com.example.test;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.util.Properties;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import org.apache.log4j.BasicConfigurator;
+
+import java.io.IOException;
+
+public class User2Application extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(User2Application.class.getResource("User2.fxml"));
+//        Scene scene1 = new Scene(fxmlLoader.load(), 392, 368);
+        Scene scene1 = new Scene(fxmlLoader.load());
+//        stage.setTitle("Hello!");
+        stage.setScene(scene1);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+        //thiết lập môi trường cho JMS
+        BasicConfigurator.configure();
+        //thiết lập môi trường cho JJNDI
+        Properties settings=new Properties();
+        settings.setProperty(Context.INITIAL_CONTEXT_FACTORY,
+                "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+        settings.setProperty(Context.PROVIDER_URL, "tcp://localhost:61616");
+        //tạo context
+        Context ctx=new InitialContext(settings);
+        //lookup JMS connection factory
+        Object obj=ctx.lookup("ConnectionFactory");
+        ConnectionFactory factory=(ConnectionFactory)obj;
+        //lookup destination
+        Destination destination
+                =(Destination) ctx.lookup("dynamicQueues/thanthidet");
+        //tạo connection
+        Connection con=factory.createConnection("admin","admin");
+        //nối đến MOM
+        con.start();
+        //tạo session
+        Session session=con.createSession(
+                /*transaction*/false,
+                /*ACK*/Session.CLIENT_ACKNOWLEDGE
+        );
+        //tạo consumer
+        MessageConsumer receiver = session.createConsumer(destination);
+        //blocked-method for receiving message - sync
+        //receiver.receive();
+        //Cho receiver lắng nghe trên queue, chừng có message thì notify - async
+        System.out.println("Tý was listened on queue...");
+        receiver.setMessageListener(new MessageListener() {
+
+            //có message đến queue, phương thức này được thực thi
+            public void onMessage(Message msg) {//msg là message nhận được
+                try {
+                    if(msg instanceof TextMessage){
+                        TextMessage tm=(TextMessage)msg;
+                        String txt=tm.getText();
+                        System.out.println("Nhận được "+txt);
+                        msg.acknowledge();//gửi tín hiệu ack
+                    }
+                    else if(msg instanceof ObjectMessage){
+                        ObjectMessage om=(ObjectMessage)msg;
+                        System.out.println(om);
+                    }
+                    //others message type....
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } }
+        });
+    }
+}
